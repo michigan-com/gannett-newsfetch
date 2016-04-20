a0d5adb53b1792ac1c5d9199410b69296e8f5d1c
Andrey Tarantsov <andrey@tarantsov.com>
2016-04-21 00:04:52 +0600

    ✂️ leftpad no more

--- gannettApi/format.go
+++ gannettApi/format.go
@@ -14,22 +14,5 @@ import (
 		https://confluence.gannett.com/pages/viewpage.action?title=Search+v4+Recipes&spaceKey=GDPDW#Searchv4Recipes-FilterbyDateRange
 */
 func FormatAsDateString(year, month, day, hour, minute, second int) string {
-	return fmt.Sprintf("%s-%s-%sT%s:%s:%sZ",
-		GetZeroPaddedString(year),
-		GetZeroPaddedString(month),
-		GetZeroPaddedString(day),
-		GetZeroPaddedString(hour),
-		GetZeroPaddedString(minute),
-		GetZeroPaddedString(second))
-}
-
-/*
-	Zero pad a number (useful for date strings, e.g. 1 -> 01 )
-*/
-func GetZeroPaddedString(i int) string {
-	if i < 10 {
-		return fmt.Sprintf("0%d", i)
-	} else {
-		return fmt.Sprintf("%d", i)
-	}
+	return fmt.Sprintf("%02d-%02d-%02dT%02d:%02d:%02dZ", year, month, day, hour, minute, second)
 }
